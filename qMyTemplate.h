6897b4e2eaddb92eef2d11ea54360beccb656a8e
bianxindong <bxd_best@163.com>
2020-03-30 15:43:08 +0800

    update class name

--- qMyTemplate.h
+++ qMyTemplate.h
@@ -2,22 +2,22 @@
 #include "VisualCoreExport.h"
 #include <QObject>
 
-class VisualTempletePrivate;
-class VISUAL_VISUALCORE_EXPORT VisualTemplete : public QObject
+class qMyTemplatePrivate;
+class VISUAL_VISUALCORE_EXPORT qMyTemplate : public QObject
 {
 	Q_OBJECT
 public:
-	VisualTemplete(QObject* parent = 0);
-	~VisualTemplete();
+	qMyTemplate(QObject* parent = 0);
+	~qMyTemplate();
 
 public slots:
 
 signals:
 
 protected:
-	QScopedPointer<VisualTempletePrivate> d_ptr;
+	QScopedPointer<qMyTemplatePrivate> d_ptr;
 
 private:
-	Q_DECLARE_PRIVATE(VisualTemplete);
-	Q_DISABLE_COPY(VisualTemplete);
+	Q_DECLARE_PRIVATE(qMyTemplate);
+	Q_DISABLE_COPY(qMyTemplate);
 };
