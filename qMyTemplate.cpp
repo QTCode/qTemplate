c9f69f5ab50c7d1722491bdd6bf187c13f61719f
bianxindong <bxd_best@163.com>
2020-03-30 15:36:58 +0800

    init

---
+++ qMyTemplate.cpp
@@ -0,0 +1,36 @@
+﻿#include "qMyTemplate.h"
+
+class qMyTemplatePrivate
+{
+	Q_DECLARE_PUBLIC(qMyTemplate);
+protected:
+	qMyTemplate* const q_ptr;
+
+public:
+	qMyTemplatePrivate(qMyTemplate* object);
+	~qMyTemplatePrivate();
+};
+
+qMyTemplatePrivate::qMyTemplatePrivate(qMyTemplate* object)
+	:q_ptr(object)
+{
+
+}
+
+qMyTemplatePrivate::~qMyTemplatePrivate()
+{
+
+}
+
+qMyTemplate::qMyTemplate(QObject* parent)
+	:QObject(parent), d_ptr(new qMyTemplatePrivate(this))
+{
+	Q_D(qMyTemplate);
+
+}
+
+qMyTemplate::~qMyTemplate()
+{
+	Q_D(qMyTemplate);
+
+}
---
+++ qMyTemplate.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "VisualCoreExport.h"
+#include <QObject>
+
+class VisualTempletePrivate;
+class VISUAL_VISUALCORE_EXPORT VisualTemplete : public QObject
+{
+	Q_OBJECT
+public:
+	VisualTemplete(QObject* parent = 0);
+	~VisualTemplete();
+
+public slots:
+
+signals:
+
+protected:
+	QScopedPointer<VisualTempletePrivate> d_ptr;
+
+private:
+	Q_DECLARE_PRIVATE(VisualTemplete);
+	Q_DISABLE_COPY(VisualTemplete);
+};
