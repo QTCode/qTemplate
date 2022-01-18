146d8f09e9d0fb45984bde4e9f008883e7017257
bianxindong <bxd_best@163.com>
2022-01-18 11:45:11 +0800

    update 引用私有类

---
+++ qMyTemplateWidget.cpp
@@ -0,0 +1,39 @@
+﻿#include "qMyTemplateWidget.h"
+#include "ui_qMyTemplateWidget.h"
+
+
+
+class qMyTemplateWidgetPrivate: public Ui_qMyTemplateWidget
+{
+	Q_DECLARE_PUBLIC(qMyTemplateWidget);
+protected:
+	qMyTemplateWidget* const q_ptr;
+
+public:
+	qMyTemplateWidgetPrivate(qMyTemplateWidget& object);
+	~qMyTemplateWidgetPrivate();
+};
+
+qMyTemplateWidgetPrivate::qMyTemplateWidgetPrivate(qMyTemplateWidget& object)
+	:q_ptr(&object)
+{
+
+}
+
+qMyTemplateWidgetPrivate::~qMyTemplateWidgetPrivate()
+{
+
+}
+
+qMyTemplateWidget::qMyTemplateWidget(QWidget* parent)
+	:QWidget(parent), d_ptr(new qMyTemplateWidgetPrivate(*this))
+{
+	Q_D(qMyTemplateWidget);
+
+}
+
+qMyTemplateWidget::~qMyTemplateWidget()
+{
+	Q_D(qMyTemplateWidget);
+
+}
---
+++ qMyTemplateWidget.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <QWidget>
+
+class qMyTemplateWidgetPrivate;
+class  qMyTemplateWidget : public QWidget
+{
+	Q_OBJECT
+public:
+	qMyTemplateWidget(QWidget* parent = 0);
+	~qMyTemplateWidget();
+
+public slots:
+
+signals:
+
+protected:
+	QScopedPointer<qMyTemplateWidgetPrivate> d_ptr;
+
+private:
+	Q_DECLARE_PRIVATE(qMyTemplateWidget);
+	Q_DISABLE_COPY(qMyTemplateWidget);
+};
